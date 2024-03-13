01e767a3589cc49082df3a28f354e1d7f4d3f6b3
chobuzz <chobuzz23@gmail.com>
2024-03-14 06:53:25 +0900

    add sidebar #12

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,7 +5,7 @@ function Sidebar() {
     <header className="relative flex flex-col w-76px h-lvh bg-dark-blue">
       <h1>쓰파인더</h1>
       <nav className="absolute h-490px w-90 left-0 bottom-0  flex flex-col rounded-tr-lg bg-white justify-between">
-        <span>123123</span>
+        <span>1231234</span>
         <span>2</span>
         <span>3</span>
         <span>4</span>
