0cbcd5fb68d048d05b7d590208f333faa7fc1c3e
leegurts <rmstjd333@gmail.com>
2024-03-14 23:54:27 +0900

    [FEAT] 마커 클릭시 이미지 출력되도록 제작
    - createNewMarker 함수를 통해 마커에 대한 정보를 담은 객체를 반환하도록
      제작
    - 객체의 정보를 통해 이미지 정보 출력.

--- src/app/location/page.tsx
+++ src/app/location/page.tsx
@@ -1,20 +1,55 @@
 "use client";
 
-import Link from "next/link";
-import { useEffect, useRef } from "react";
+import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
+
+interface TrashCan {
+  lat: number;
+  lng: number;
+  status: "added" | "waiting" | "removed";
+  image?: string;
+  description?: string;
+}
 
 const randomLatLng = () =>
   // 서울의 위도 경도 범위
-  Array(1000)
+  Array(100)
     .fill(0)
     .map(() => {
       const lat = Math.random() * (37.7 - 37.4) + 37.4;
       const lng = Math.random() * (127.2 - 126.8) + 126.8;
-      return new window.kakao.maps.LatLng(lat, lng);
+      return { lat, lng };
     });
+
+const createNewMarker = (
+  { lat, lng, status, image, description }: TrashCan,
+  setSelectedMarker: React.Dispatch<React.SetStateAction<TrashCan | null>>,
+) => {
+  const icon =
+    status === "added" ? "/img/TrashCanIMG.png" : "/img/TrashCanIMG.png";
+  const markerImage = new window.kakao.maps.MarkerImage(
+    icon,
+    new window.kakao.maps.Size(30, 30),
+    {
+      offset: new window.kakao.maps.Point(15, 15),
+    },
+  );
+  const marker = new window.kakao.maps.Marker({
+    position: new window.kakao.maps.LatLng(lat, lng),
+    image: markerImage,
+  });
+
+  window.kakao.maps.event.addListener(marker, "click", () => {
+    setSelectedMarker({ lat, lng, status, image, description });
+  });
+
+  return { lat, lng, status, marker, image, description };
+};
 
 export default function LocationPage() {
   const mapRef = useRef<HTMLDivElement | null>(null);
+  const [selectedMarker, setSelectedMarker] = useState<TrashCan | null>(null);
+
   useEffect(() => {
     if (window.kakao) {
       window.kakao.maps.load(() => {
@@ -31,27 +66,38 @@ export default function LocationPage() {
         );
 
         const clusterer = new window.kakao.maps.MarkerClusterer({
-          map: mapRef.current,
+          map,
           averageCenter: true,
           minLevel: 7,
         });
 
-        const positions = randomLatLng().map(
-          (position) =>
-            new window.kakao.maps.Marker({
-              position,
-            }),
+        const positions = randomLatLng().map((position) =>
+          createNewMarker(
+            {
+              lat: position.lat,
+              lng: position.lng,
+              status: "added",
+              image: "/img/TEST.jpg",
+            },
+            setSelectedMarker,
+          ),
         );
 
-        clusterer.addMarkers(positions);
+        clusterer.addMarkers(positions.map((position) => position.marker));
       });
     }
   }, []);
 
   return (
     <div className="flex flex-col">
-      <div ref={mapRef} style={{ width: "100%", height: "90vh" }} />
-      <Link href="/login">로그인</Link>
+      <div ref={mapRef} className="w-screen h-screen" />
+      <Image
+        className="absolute left-0 top-0 z-50"
+        width={500}
+        height={300}
+        src={selectedMarker?.image ?? ""}
+        alt=""
+      />
     </div>
   );
 }
