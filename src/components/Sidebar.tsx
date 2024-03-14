1853c85777dc25319ccf0ed295bb4764534e9710
chobuzz <chobuzz23@gmail.com>
2024-03-14 23:13:02 +0900

    fix sidebar with feedback

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function Sidebar() {
   return (
-    <header className="relative flex flex-col w-76px h-lvh bg-dark-blue">
-      <h1>쓰파인더</h1>
-      <nav className="absolute h-490px w-90 left-0 bottom-0  flex flex-col rounded-tr-lg bg-white justify-between">
+    <header className="flex flex-col w-[4.25rem] h-lvh bg-dark-blue">
+      <h1 className="aspect-[3/5] w-full">쓰파인더</h1>
+      <nav className=" w-full flex flex-col flex-grow rounded-tr-lg bg-white space-y-1">
         <span>123123</span>
         <span>2</span>
         <span>3</span>
