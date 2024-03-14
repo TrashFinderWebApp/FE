b592d58589c74f1fb8905c429bfc9ab11f12a213
chobuzz <chobuzz23@gmail.com>
2024-03-15 05:04:30 +0900

    [FIX] SVG 컴포넌트화 해서 사용 시도 - 하지만 잘 안됐음.. 왜 적용이 안되는지 모르겠어!!

--- src/components/sidebar/Navigator.tsx
+++ src/components/sidebar/Navigator.tsx
@@ -1,14 +1,36 @@
+import Image from "next/image";
+import HomeSVG from "../svg/HomeSVG";
+import RegisterSVG from "../svg/RegisterSVG";
+
 export default function Navigator() {
   return (
     <header className="flex flex-col w-[4.25rem] h-lvh bg-dark-blue">
       <h1 className="aspect-[3/5] w-full">쓰파인더</h1>
-      <nav className=" w-full flex flex-col flex-grow rounded-tr-lg bg-white space-y-1">
-        <ul>
-          <li>1</li>
-          <li>2</li>
-          <li>3</li>
-          <li>4</li>
-          <li>5</li>
+      <nav className=" w-full flex flex-col flex-grow rounded-tr-lg bg-white ">
+        <ul className="flex flex-col items-center space-y-6">
+          <li className="mt-6">
+            <HomeSVG
+              width="32"
+              height="46"
+              color="#05668D"
+              className="w-[100px]"
+            />
+          </li>
+          <li>
+            <Image src="/svg/Home.svg" alt="Home" width={32} height={46} />
+          </li>
+          <li>
+            <RegisterSVG width="32" height="46" color="black" />
+          </li>
+          <li>
+            <Image src="/svg/Home.svg" alt="Home" width={32} height={46} />
+          </li>
+          <li>
+            <Image src="/svg/Home.svg" alt="Home" width={32} height={46} />
+          </li>
+          <li>
+            <Image src="/svg/Home.svg" alt="Home" width={32} height={46} />
+          </li>
         </ul>
       </nav>
     </header>
---
+++ src/types/svgprops.d.ts
@@ -0,0 +1,6 @@
+export interface SVGProps {
+  width: string;
+  height: string;
+  color?: string;
+  className?: string;
+}
