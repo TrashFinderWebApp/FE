5a194ee7c9a182fc47955e8117d459bbd2a36790
chobuzz <chobuzz23@gmail.com>
2024-03-15 02:14:37 +0900

    [FIX] 피드백 반영해서 다시 올렸음!!

--- src/components/sidebar/Navigator.tsx
+++ src/components/sidebar/Navigator.tsx
@@ -1,12 +1,15 @@
-export default function SideBar() {
+export default function Navigator() {
   return (
     <header className="flex flex-col w-[4.25rem] h-lvh bg-dark-blue">
       <h1 className="aspect-[3/5] w-full">쓰파인더</h1>
       <nav className=" w-full flex flex-col flex-grow rounded-tr-lg bg-white space-y-1">
-        <span>123123</span>
-        <span>2</span>
-        <span>3</span>
-        <span>4</span>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+          <li>5</li>
+        </ul>
       </nav>
     </header>
   );
